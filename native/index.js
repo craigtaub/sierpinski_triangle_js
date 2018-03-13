2bc7e59f6f96936c4a27f9365c56887c9ea52878
Craig Taub <craigtaub@gmail.com>
2018-03-13 15:47:17 +0000

    basic working

---
+++ native/index.js
@@ -0,0 +1,85 @@
+console.log('sierpinski_triangle:')
+
+const targetSize = 31;
+let scale = 1;
+// const targetSize = 1;
+
+function dot(props) {
+  // document.getElementById('app').style.transform = 'scaleX(' + (scale / 2.1) + ') scaleY(0.7) translateZ(0.1px)';
+  document.getElementById('app').innerHTML += "<span class='dot' style='left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>";
+}
+
+function triangle(props) {
+  let s = props.s;
+
+  if (s <= targetSize) {
+    return dot({
+      x: props.x - (targetSize / 2),
+      y: props.y - (targetSize / 2),
+      size: targetSize,
+      text: props.seconds,
+    });
+  }
+
+  s = s / 2;
+
+  triangle({
+    x: props.x,
+    y: props.y - (s / 2),
+    s: s,
+    seconds: props.seconds
+  }),
+  triangle({
+    x: props.x - s,
+    y: props.y + (s / 2),
+    s: s,
+    seconds: props.seconds
+  }),
+  triangle({
+    x: props.x + s,
+    y: props.y + (s / 2),
+    s: s,
+    seconds: props.seconds
+  });
+
+}
+
+function app() {
+  let seconds = 0;
+  let elapsed;
+  const start = Date.now();
+
+  function nextFrame() {
+    elapsed = Date.now() - start;
+    const t = (elapsed / 1000) % 10;
+    scale = 1 + (t > 5 ? 10 - t : t) / 10;
+
+    window.requestAnimationFrame(() => {
+      nextFrame();
+    });
+  };
+  function nextSecond() {
+    seconds = (seconds > 9) ? 0 : seconds + 1;
+    setTimeout(() => {
+      nextSecond();
+      topTriangle(seconds);
+    }, 1000);
+  }
+
+  function topTriangle(seconds) {
+    document.getElementById('app').innerHTML = '';
+    triangle({
+      x: 0,
+      y: 0,
+      s: 1000, 
+      seconds
+    });
+  }
+  
+  nextFrame();
+  nextSecond();
+
+  topTriangle(seconds);
+}
+
+app();
