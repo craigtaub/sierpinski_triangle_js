1b976505f85d2bc87868f812802ced7bfdc6254a
Craig Taub <craigtaub@gmail.com>
2018-04-02 19:45:00 +0100

    add css + basic notes

--- native/index.js
+++ native/index.js
@@ -1,20 +1,25 @@
 console.log('sierpinski_triangle:')
 
+// file:///Users/craigtaub/Documents/javascript/delete/sierpinski_triangle/native/index.html
+
+
 let dots = [];
 
-const targetSize = 31;
-// let scale = 1;
+const targetSize = 31; // 35fps
+// const targetSize = 15.625; // 12fps
+let scale = 1;
 // const targetSize = 1;
 
 function dot(props) {
-  // document.getElementById('app').style.transform = 'scaleX(' + (scale / 2.1) + ') scaleY(0.7) translateZ(0.1px)';
+  // document.getElementById('app').innerHTML += "<span class='dot' style='transform: scaleX(" + (scale / 2.1) + ") scaleY(0.7) translateZ(0.1px); left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>";
   // document.getElementById('app').innerHTML += "<span class='dot' style='left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>";
-  dots.push("<span class='dot' style='left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>");
+  // dots.push("<span class='dot' style='left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>");
+  
+  dots.push("<span class='dot' style='transform: scaleX(" + (scale / 2.1) + ") scaleY(0.7) translateZ(0.1px); left: " + props.x + "; top: " + props.y + "; padding-left: " + 2 * props.text + "px'>" + props.text + "</span>");
 }
 
 function triangle(props) {
   let s = props.s;
-
   if (s <= targetSize) {
     return dot({
       x: props.x - (targetSize / 2),
@@ -49,22 +54,26 @@ function triangle(props) {
 
 function app() {
   let seconds = 0;
-  // let elapsed;
-  // const start = Date.now();
+  let elapsed;
+  const start = Date.now();
 
   function nextFrame() {
-    // elapsed = Date.now() - start;
-    // const t = (elapsed / 1000) % 10;
-    // scale = 1 + (t > 5 ? 10 - t : t) / 10;
+    elapsed = Date.now() - start;
+    const t = (elapsed / 1000) % 10;
+    scale = 1 + (t > 5 ? 10 - t : t) / 10;
 
     window.requestAnimationFrame(() => { 
+    // setTimeout(() => {
       // TELL browser want to perform animation, will call this BEFORE it calls next repaint.
         // i.e. batch all actions in here for single repaint/reflow + less DOM throttle.
       // USE to interact with CSS/HTML via JS.
       // CAN call 60 times per second BUT has intelligence to match display refresh rate in browser.
+
+      // 12fps
       const string = dots.reduce((acc, curr) => curr += acc, '');
       document.getElementById('app').innerHTML = string;
       nextFrame();
+    // }, 1000);
     });
   };
   function nextSecond() {
@@ -72,16 +81,20 @@ function app() {
     setTimeout(() => {
       nextSecond();
       topTriangle(seconds);
+
+      // 1fps
+      // const string = dots.reduce((acc, curr) => curr += acc, '');
+      // document.getElementById('app').innerHTML = string;
     }, 1000);
   }
 
   function topTriangle(seconds) {
     dots = [];
-    // document.getElementById('app').innerHTML = ''; // reset
+    document.getElementById('app').innerHTML = ''; // reset
     triangle({
       x: 0,
       y: 0,
-      s: 1000, 
+      s: 600, // 1000
       seconds
     });
   }
